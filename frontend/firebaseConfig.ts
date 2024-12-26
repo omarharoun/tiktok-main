c64fcf893dce604ad10e011dec141ba5a671077b
fares haroun <far0213025@ju.edu.jo>
2024-12-26 22:02:31 +0300

    Add initial project configuration and dependencies

--- frontend/firebaseConfig.ts
+++ frontend/firebaseConfig.ts
@@ -3,19 +3,35 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-//! REPLACE VALUES BELOW WITH YOUR OWN FIREBASE CONFIG
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from "@env";
+
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "*****",
-  authDomain: "*****",
-  databaseURL: "*****",
-  projectId: "*****",
-  storageBucket: "*****",
-  messagingSenderId: "*****",
-  appId: "*****",
-  measurementId: "*****",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication with persistence
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+
+// Initialize Firestore and Storage
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
 export const FIREBASE_STORAGE = getStorage(FIREBASE_APP);
